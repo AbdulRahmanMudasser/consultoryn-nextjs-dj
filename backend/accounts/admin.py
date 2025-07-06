f836a6126d8cebc7761d8e77572e1f931b4d9899
Abdul Rahman <102192151+AbdulRahmanMudasser@users.noreply.github.com>
2025-07-06 16:01:43 +0500

    backend-chore: registered custom user admin in admin.py and modified apps.py

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, PasswordResetToken
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ['email', 'username', 'role', 'is_active', 'date_joined']
+    list_filter = ['role', 'is_active']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('username', 'role')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'password1', 'password2', 'role', 'is_active', 'is_staff', 'is_superuser')}
+        ),
+    )
+    search_fields = ['email', 'username']
+    ordering = ['email']
+    filter_horizontal = ('groups', 'user_permissions',)
+
+admin.site.register(User, CustomUserAdmin)
+admin.site.register(PasswordResetToken)
